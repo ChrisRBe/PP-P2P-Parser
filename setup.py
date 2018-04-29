a8cfb815b4e634caf0de1da4f473b23360120e0c
ChristRBe <chrisrbe@outlook.com>
2018-04-30 00:16:14 +0200

    Add requirements.txt
    Add requirements.txt and setup.py.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='PP-P2P-Parser',
+    version='0.2',
+    packages=['src', 'src.test'],
+    url='https://github.com/ChrisRBe/PP-P2P-Parser',
+    license='GPL-v3',
+    author='ChrisRBe',
+    author_email='chrisrbe@outlook.com',
+    description='Parser for P2P services like mintos.com for Portfolio Performance.'
+)
