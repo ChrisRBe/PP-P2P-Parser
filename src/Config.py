19967c8dc0505db25bb60d369824ab03dfa3c40c
tobi.butch <tobias.metzger@gmx.de>
2018-10-15 16:58:16 +0200

    made instance variables non-public

--- src/Config.py
+++ src/Config.py
@@ -17,65 +17,65 @@ class Config():
         Constructor for Config
         """
         logging.info("Config ini")
-        self.relevant_invest_regex = re.compile(config['type_regex']['deposit'])
-        self.relevant_payment_regex = re.compile(config['type_regex']['withdraw'])
-        self.relevant_income_regex = re.compile(config['type_regex']['interest'])
+        self._relevant_invest_regex = re.compile(config['type_regex']['deposit'])
+        self._relevant_payment_regex = re.compile(config['type_regex']['withdraw'])
+        self._relevant_income_regex = re.compile(config['type_regex']['interest'])
         if 'fee' in config['type_regex']:
-            self.relevant_fee_regex = re.compile(config['type_regex']['fee'])
+            self._relevant_fee_regex = re.compile(config['type_regex']['fee'])
         else:
-            self.relevant_fee_regex = None
+            self._relevant_fee_regex = None
 
-        self.booking_date = config['csv_fieldnames']['booking_date']
-        self.booking_date_format = config['csv_fieldnames']['booking_date_format']
-        self.booking_details = config['csv_fieldnames']['booking_details']
-        self.booking_id = config['csv_fieldnames']['booking_id']
-        self.booking_type = config['csv_fieldnames']['booking_type']
-        self.booking_value = config['csv_fieldnames']['booking_value']
+        self._booking_date = config['csv_fieldnames']['booking_date']
+        self._booking_date_format = config['csv_fieldnames']['booking_date_format']
+        self._booking_details = config['csv_fieldnames']['booking_details']
+        self._booking_id = config['csv_fieldnames']['booking_id']
+        self._booking_type = config['csv_fieldnames']['booking_type']
+        self._booking_value = config['csv_fieldnames']['booking_value']
         if 'booking_currency' in config['csv_fieldnames']:
-            self.booking_currency = config['csv_fieldnames']['booking_currency']
+            self._booking_currency = config['csv_fieldnames']['booking_currency']
         else:
-            self.booking_currency = ''
+            self._booking_currency = ''
 
     def get_relevant_invest_regex(self):
         """ get the relevant_invest_regex """
-        return self.relevant_invest_regex
+        return self._relevant_invest_regex
 
     def get_relevant_payment_regex(self):
         """ get the relevant_payment_regex """
-        return self.relevant_payment_regex
+        return self._relevant_payment_regex
 
     def get_relevant_income_regex(self):
         """ get the relevant_income_regex """
-        return self.relevant_income_regex
+        return self._relevant_income_regex
 
     def get_relevant_fee_regex(self):
         """ get the relevant_fee_regex """
-        return self.relevant_fee_regex
+        return self._relevant_fee_regex
 
     def get_booking_date(self):
         """ get the booking_date """
-        return self.booking_date
+        return self._booking_date
 
     def get_booking_date_format(self):
         """ get the booking_date_format """
-        return self.booking_date_format
+        return self._booking_date_format
 
     def get_booking_details(self):
         """ get the booking_details """
-        return self.booking_details
+        return self._booking_details
 
     def get_booking_id(self):
         """ get the booking_id """
-        return self.booking_id
+        return self._booking_id
 
     def get_booking_type(self):
         """ get the booking_type """
-        return self.booking_type
+        return self._booking_type
 
     def get_booking_value(self):
         """ get the booking_value """
-        return self.booking_value
+        return self._booking_value
 
     def get_booking_currency(self):
         """ get the booking_currency """
-        return self.booking_currency
+        return self._booking_currency
--- src/Statement.py
+++ src/Statement.py
@@ -16,7 +16,7 @@ class Statement():
         """
         Constructor for Statement
         """
-        self.config = config
+        self._config = config
         self._statement = statement
 
     def get_category(self):
@@ -25,13 +25,13 @@ class Statement():
 
         :return: category of the statement; if unkown return the empty string
         """
-        booking_type = self._statement[self.config.booking_type]
+        booking_type = self._statement[self._config.get_booking_type()]
         category = ""
-        if self.config.get_relevant_income_regex().match(booking_type):
+        if self._config.get_relevant_income_regex().match(booking_type):
             category = 'Zinsen'
-        elif self.config.get_relevant_invest_regex().match(booking_type):
+        elif self._config.get_relevant_invest_regex().match(booking_type):
             category = 'Einlage'
-        elif self.config.get_relevant_payment_regex().match(booking_type):
+        elif self._config.get_relevant_payment_regex().match(booking_type):
             category = 'Entnahme'
         elif self.is_fee(booking_type):
             category = 'Gebühren'
@@ -41,20 +41,20 @@ class Statement():
 
     def is_fee(self, booking_type):
         """ check if it is a statement with a fee"""
-        return self.config.get_relevant_fee_regex() and self.config.get_relevant_fee_regex().match(booking_type)
+        return self._config.get_relevant_fee_regex() and self._config.get_relevant_fee_regex().match(booking_type)
 
     def get_date(self):
         """ get the date of the statement """
-        return datetime.strptime(self._statement[self.config.get_booking_date()], self.config.get_booking_date_format()).date()
+        return datetime.strptime(self._statement[self._config.get_booking_date()], self._config.get_booking_date_format()).date()
 
     def get_value(self):
         """ get the value of the statement """
-        return self._statement[self.config.get_booking_value()].replace('.', ',')
+        return self._statement[self._config.get_booking_value()].replace('.', ',')
 
     def get_note(self):
         """ get the note of the statement """
-        return "{id}: {details}".format(id=self._statement[self.config.get_booking_id()],
-                                        details=self._statement[self.config.get_booking_details()])
+        return "{id}: {details}".format(id=self._statement[self._config.get_booking_id()],
+                                        details=self._statement[self._config.get_booking_details()])
 
     def get_currency(self):
         """
@@ -62,7 +62,7 @@ class Statement():
 
         :return: currency of the statement; if unkown return 'EUR'
         """
-        if self.config.get_booking_currency():
-            return self._statement[self.config.get_booking_currency()]
+        if self._config.get_booking_currency():
+            return self._statement[self._config.get_booking_currency()]
         else:
             return 'EUR'
