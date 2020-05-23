61f0dec534bcbf9954343be87202c222ea7eef65
ChrisRBe <ChrisRBe@outlook.com>
2020-05-23 17:29:46 +0200

    Fix mintos parsing to recognize new format of account statement
    Signed-off-by: ChrisRBe <ChrisRBe@outlook.com>

--- src/Statement.py
+++ src/Statement.py
@@ -8,7 +8,7 @@ import logging
 from datetime import datetime
 
 
-class Statement():
+class Statement:
     """
     Implementation of the statement
     """
--- src/test/test_portfolio_performance_writer.py
+++ src/test/test_portfolio_performance_writer.py
@@ -9,8 +9,8 @@ import os
 import tempfile
 from unittest import TestCase
 
-from ..portfolio_performance_writer import PortfolioPerformanceWriter
-from ..portfolio_performance_writer import PP_FIELDNAMES
+from src.portfolio_performance_writer import PortfolioPerformanceWriter
+from src.portfolio_performance_writer import PP_FIELDNAMES
 
 
 class TestPortfolioPerformanceWriter(TestCase):
